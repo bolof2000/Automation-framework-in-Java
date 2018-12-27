9554c206da8ebc54950abc9f1a2a2351b63463f5
bolof <bolof@bolofs-MacBook-Pro.local>
2018-12-26 19:15:58 -0700

    adding data Provider to your framework

--- src/main/java/com/automation/pages/Pages.java
+++ src/main/java/com/automation/pages/Pages.java
@@ -6,14 +6,47 @@ the page class contains the definitions of objects that are common acrors
 all pages and this can be inherited by every other page
  */
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
 
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 public class Pages {
 
     public static WebDriver driver;
+    public static Properties OR = new Properties();
+    public static Properties config = new Properties();
+    public static FileInputStream fis;
+    public static Logger log = Logger.getLogger("");
+    //public static ExcelReader excel = new ExcelReader()
+    public static WebDriverWait wait;
+    //public ExtentReports rep = Ext
+    public static ExtentTest test;
+    public static String browser;
+
+    @DataProvider(name="dp")
+    public Object[][] getData(Method m){
+
+            String sheetName = m.getName();
+            //int rows =
+
+        return null;
+
+
+
+
+
+    }
+
+
 
     public Pages(){
 
